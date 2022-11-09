0873c256bf74dac9956f188ed4882a597b58c1b8
tid213 <tid2136141@gmail.com>
2022-11-09 16:19:49 -0700

    added post route to signup, sends data successfully!

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const { createClient } = require('@supabase/supabase-js');
+
+const supabase = createClient('https://lehszutacgaprmdvkcdf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxlaHN6dXRhY2dhcHJtZHZrY2RmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc1MTcwODEsImV4cCI6MTk4MzA5MzA4MX0.-7dOqj8q6O53GVqCcuWcGseahmFQZKT71HwMdGz_V24')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  const message = "hello";
+router.get('/', async (req, res, next) => {
+  const { data, error } = await supabase
+  .from('testing')
+  .select()
+  const message = data;
   res.send(message);
 });
 
@@ -16,6 +22,9 @@ router.get('/signup', function(req, res, next) {
   const message = "Sign up for Green Notez";
   res.send(message);
 });
+router.post('/signup', (req, res, next) => {
+  console.log(req.body);
+})
 
 router.get('/about', function(req, res, next) {
   const message = "About page";
